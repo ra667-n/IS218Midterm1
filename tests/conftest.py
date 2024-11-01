137d3ff7f1ec4980d7151fc86f5418cbf32eaa70
Roheemat Adebiyi <ra667@njit.edu>
2024-11-01 19:47:03 -0400

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from decimal import Decimal
 from faker import Faker
-from calculator.operations import add, subtract, multiply, divide
+from calculaton.operations import add, subtract, multiply, divide
 
 fake = Faker()
 
